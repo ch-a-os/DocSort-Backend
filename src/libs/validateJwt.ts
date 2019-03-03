63c77e2b83b5eb5613daf90bf0dd271b56539f15
Nicolas <emailchecker000@gmail.com>
2019-03-03 17:30:52 +0100

    * Now, token can be served by query as fallback

--- src/libs/validateJwt.ts
+++ src/libs/validateJwt.ts
@@ -6,15 +6,19 @@ import { config } from '../config';
  * Express Middleware to validate user jwt-keys
  */
 export function validateJWT(req: Request, res: Response, next: Function): boolean | void {
-    if(req.headers.token == null) {
+    let token = "";
+
+    if(req.headers.token == null && req.query.token == null) {
         res.status(401).send({error: "Sorry, but you forgot to give me your token."});
         return false;
     }
+    token = req.headers.token != null? req.headers.token : req.query.token;
+    
     try {
-        jwt.verify(req.headers.token.toString(), config.secretJWT);
+        jwt.verify(token, config.secretJWT);
         next(); // Token is vaild.
     } catch(err) {
         if(err) console.error(err);
-        res.status(401).send({error: "Sorry, but your token you gave me is not vaild."})
+        res.status(401).send({error: "Sorry, but that token you gave me is not vaild."})
     }
 }
